535689e449b0e7eee9ddf78e71776883af4cd2a0
Brian Otieno <jumaxbrian@gmail.com>
2018-01-15 20:24:59 +0300

    adding product type, removing logs, add comments

--- cart.js
+++ cart.js
@@ -1,9 +1,4 @@
-// alert('hello cart!');
-// var cart = document.getElementsByClassName("cart");
-// cart = cart[0];
-// cart.style.visibility = 'hidden';
-
-// var addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+//declare nodes for the various buttons which need listeners
 var products = document.getElementsByClassName("description");
 const tbody = document.getElementsByTagName('tbody')[0];
 const keepShoppingBtn = document.getElementById("keep-shopping");
@@ -14,17 +9,15 @@ const toggleCartImg = document.getElementById("toggle-cart-img");
 
 
 var cart = {
-    cartItems: [],
+
+    cartItems: [],  //array containing cart items
+
+    //add an item object to the cart
     addCartItem: function (cartItem) {
         this.cartItems.push(cartItem);
     },
 
-    printItems: function () {
-        for (var i = 0; i < this.cartItems.length; i++) {
-            console.log(JSON.stringify(this.cartItems));
-        }
-    },
-
+    //calculate total cost of cart items
     updateCalculation() {
         var total = 0;
         for (var i = 0; i < this.cartItems.length; i++) {
@@ -34,40 +27,41 @@ var cart = {
         return total;
     },
 
+    //toggle shopping cart based on number of items in the cart
     toggleCart() {
         var cart = document.getElementsByClassName("cart");
         cart = cart[0];
 
         if (this.cartItems.length > 0) {
-            // cart.style.visibility = 'visible';
             showCartDiv();
         } else {
-            // cart.style.visibility = 'hidden';
             hideCartDiv();
         }
     },
 
+    //Build up the shopping card display
     updateDisplay() {
         tbody.innerHTML = "";
         for (let i = 0; i < cart.cartItems.length; i++) {
             let newRow = tbody.insertRow(i),
                 cartItem = cart.cartItems[i];
 
-            var tempStr = '<div class="image"><img src="http://via.placeholder.com/100x100" alt="product"></div > '
-
+            // Insert a cell in the row at index 0 
+            var tempStr = '<div class="image"><img src="http://via.placeholder.com/100x100" alt="product"></div > ';
             var newCell = newRow.insertCell(0);
             newCell.innerHTML = tempStr;
 
+            // Insert a cell in the row at index 1
             tempStr = '\
             <p class="bold">' + cartItem.name + '</p>\
+            <br/><p class="bold-product-type">' + cartItem.type + '</p>\
             <p class="blue">' + cartItem.description + '</p>    \
-            '
+            ';
 
-            // // Insert a cell in the row at index 0 status
             var newCell = newRow.insertCell(1);
-            newCell.innerHTML = tempStr;;
+            newCell.innerHTML = tempStr;
 
-            // // Insert a cell in the row at index 0 status
+            // Insert a cell in the row at index 2
             var newCell = newRow.insertCell(2);
             let sel = document.createElement("select"),
                 selDiv = document.createElement("div"),
@@ -78,6 +72,7 @@ var cart = {
                 opt4 = document.createElement("option"),
                 opt5 = document.createElement("option");
 
+            //assign values and texts to options
             opt0.value = "0";
             opt0.text = "0";
             opt1.value = "1";
@@ -91,6 +86,7 @@ var cart = {
             opt5.value = "5";
             opt5.text = "5";
 
+            //add options to select object
             sel.add(opt0);
             sel.add(opt1);
             sel.add(opt2);
@@ -98,34 +94,27 @@ var cart = {
             sel.add(opt4);
             sel.add(opt5);
 
-            //just to prevent array bounds error on selected quantities.
+            //add attributes to select object
             sel.selectedIndex = cartItem.quantity;
-            // if (cartItem.quantity > 5) {
-            //     sel.selectedIndex = 6;
-            // } else {
-
-            // }
-
             sel.setAttribute("id", "quantity");
             sel.onchange = (function (newRow, cartItemIndex) { return function (event) { quantityHandler(event, cartItemIndex, newRow); } })(newRow, i);
-            selDiv.className = "drop-down";
 
+            selDiv.className = "drop-down";
             selDiv.appendChild(sel);
             newCell.appendChild(selDiv);
-            // newCell.innerHTML = tempStr;
 
+            // Insert a cell in the row at index 3
             tempStr = '\
             <p id="unit-price" class="maroon"> Unit: $' + cartItem.price + '</p><br />\
             <p id="subtotal" class="maroon">Subtotal: $' + cartItem.price * cartItem.quantity + '</p>\
-            '
+            ';
 
-            // // Insert a cell in the row at index 0 status
             var newCell = newRow.insertCell(3);
             newCell.innerHTML = tempStr;
 
-            tempStr = '<button class="remove">Remove</button>'
+            // Insert a cell in the row at index 4
+            tempStr = '<button class="remove">Remove</button>';
 
-            // // Insert a cell in the row at index 0 status
             var newCell = newRow.insertCell(4);
             var btn = document.createElement('input');
             btn.type = "button";
@@ -136,36 +125,29 @@ var cart = {
 
         }
 
-        //update calculation display
+
         cart.updateDisplayPrices(null);
     },
 
+    //update prices displayed on the shopping cart view
     updateDisplayPrices(newPrice) {
         let subTotalPriceNode = document.getElementById("subtotal-price");
 
         if (newPrice === null) {
-            subTotalPriceNode.innerHTML = '$ ' + cart.updateCalculation();
+            subTotalPriceNode.innerHTML = '$ ' + cart.updateCalculation().toFixed(2);
         } else {
-            subTotalPriceNode.innerHTML = '$ ' + newPrice;
+            subTotalPriceNode.innerHTML = '$ ' + newPrice.toFixed(2);
         }
     }
 
 }
 
 
-
-
-
 for (let i = 0; i < products.length; i++) {
     let addToCartBtn = products[i].lastChild.previousSibling;
     addToCartBtn.addEventListener("click", function () {
-        console.log('Hello ', i);
-        console.log("node details ", products[i])
         let cartItem = getCartItemFromNode(products[i]);
-        console.log('typeof cart item ', typeof cartItem.price)
         cart.addCartItem(cartItem);
-
-        console.log(cart.updateCalculation());
         cart.updateDisplay();
         cart.toggleCart();
     })
@@ -173,12 +155,13 @@ for (let i = 0; i < products.length; i++) {
 
 function getCartItemFromNode(node) {
     let productName = node.firstChild.nextSibling,
-        productPrice = productName.nextSibling.nextSibling,
+        productType = productName.nextSibling.nextSibling,
+        productPrice = productType.nextSibling.nextSibling,
         productDescription = productPrice.nextSibling.nextSibling;
-    console.log('product name ', productName.innerText, productPrice.innerText, productDescription.innerText);
 
     let cartItem = {
         name: productName.innerText,
+        type: productType.innerText,
         price: parseFloat(productPrice.innerText.substring(1)),
         quantity: 1,
         description: productDescription.innerText,
@@ -212,16 +195,17 @@ the value from the cartItem property of the cart object. After that, the updateD
 called which recreates the cart UI with the relevant updates. It doesn't delete any UI item.
 */
 function deleteNode(node, cartItemIndex) {
-    // node.innerHTML = "";
+
     //delete the selected cart item
     if (cartItemIndex > -1) {
         cart.cartItems.splice(cartItemIndex, 1);
     }
-    // delete cart.cartItems[cartItemIndex];
+
     cart.updateDisplay();
     cart.toggleCart();
 }
 
+//Handler for product quantity selections i.e. 0 -> hide cart, >=1 update price
 function quantityHandler(event, cartItemIndex, node) {
     let selectValue = parseInt(event.target.value);
 
@@ -230,22 +214,20 @@ function quantityHandler(event, cartItemIndex, node) {
     } else {
         let cartItem = cart.cartItems[cartItemIndex];
         cartItem.quantity = selectValue;
-
-        // cart.cartItems[cartItemIndex].quantity = selectValue;
-        console.log(node);
-        console.log(node.querySelector("p#subtotal"));
         let total = cartItem.quantity * cartItem.price;
         updateSubtotal(node, total);
     }
 }
 
+// updates the total price on the cart display
 function updateSubtotal(rowNode, newPriceSubtotal) {
     let subtotalNode = rowNode.querySelector("p#subtotal");
     subtotalNode.innerHTML = "Subtotal: $ " + newPriceSubtotal;
     cart.updateDisplayPrices(null);
-    // console.log('cartItemsArray', JSON.stringify(cart.cartItems))
+
 }
 
+//Determines whether the proposed coupon is better/cheaper for the customer than the already applied one
 function isBetterCoupon(nextDiscount) {
     let ans = false;
     if (cart.couponDiscount >= nextDiscount) {
@@ -270,12 +252,14 @@ toggleCartImg.addEventListener("click", function () {
 
 
 applyCouponBtn.addEventListener("click", function () {
-    //get coupon
-    let couponNode = document.getElementById("coupon"),
-        couponValue = couponNode.value.toUpperCase(),
-        couponInfoNode = document.getElementById("coupon-info"),
-        productTypeValue = document.getElementById("product-type").value;
 
+    //get coupon, product type(or name for one item coupon), and coupon info node for displaying discount amounts
+    let couponNode = document.getElementById("coupon"),
+        couponValue = couponNode.value.toUpperCase().trim(),
+        couponInfoNode = document.getElementById("coupon-info"),
+        productTypeValue = document.getElementById("product-type").value.trim();
+
+    //ensure product type/name is provided for all other types of coupons exept TOTALORDER
     if ((couponValue !== 'TOTALORDER') && (productTypeValue.length < 3)) {
         alert("Please enter a relevant product type.");
     }
@@ -283,27 +267,28 @@ applyCouponBtn.addEventListener("click", function () {
     if (couponValue === 'TOTALORDER') {
         let totalPrice = cart.updateCalculation(),
             couponDiscount = 0.05 * totalPrice,
-            couponInfo = "5% discount on total order is " + couponDiscount;
+            couponInfo = "5% discount on total order is $ " + couponDiscount;
 
         let isCheaper = isBetterCoupon(couponDiscount);
         if (!isCheaper) {
-            return;
+            return; //i.e. no need to replace the current coupon with the new one
         }
 
         cart.couponApplied = true;
         cart.couponDiscount = couponDiscount;
         cart.couponName = 'TOTALORDER';
         cart.updateDisplayPrices(totalPrice - couponDiscount);
-
         couponInfoNode.innerHTML = couponInfo;
 
     } else if (couponValue === 'ONEITEM') {
         for (let i = 0; i < cart.cartItems.length; i++) {
             let cartItem = cart.cartItems[i];
+
+            //find whether the given item is in the cart
             if (cartItem.name.toUpperCase() === productTypeValue.toUpperCase()) {
                 let totalPrice = cart.updateCalculation(),
                     couponDiscount = 0.1 * cartItem.price,
-                    couponInfo = "10% discount on a single item " + cartItem.name + " is " + couponDiscount;
+                    couponInfo = "10% discount on a single item " + cartItem.name + " is $ " + couponDiscount;
 
                 let isCheaper = isBetterCoupon(couponDiscount);
                 if (!isCheaper) {
@@ -319,7 +304,7 @@ applyCouponBtn.addEventListener("click", function () {
 
                 break;
             } else {
-                alert("There is no product type " + productTypeValue + " in your cart items.")
+                alert("There is no product named " + productTypeValue + " in your cart items.");
             }
         }
 
@@ -329,19 +314,20 @@ applyCouponBtn.addEventListener("click", function () {
         for (let i = 0; i < cart.cartItems.length; i++) {
             let cartItem = cart.cartItems[i];
 
-            if (cartItem.name.toUpperCase() === productTypeValue.toUpperCase()) {
+            //find whether the given product type exists in the cart
+            if (cartItem.type.toUpperCase() === productTypeValue.toUpperCase()) {
                 productTypeTotal += cartItem.price * cartItem.quantity;
 
             } else {
-                alert("There is no product type " + productTypeValue + " in your cart items.")
+                alert("There is no product type " + productTypeValue + " in your cart items.");
             }
         }
 
-
+        //calculate discount if items of a particular product type have a cost greater than 0
         if (productTypeTotal > 0) {
             let totalPrice = cart.updateCalculation(),
                 couponDiscount = 0.15 * productTypeTotal,
-                couponInfo = "15% discount on all items " + productTypeValue + " is " + couponDiscount;
+                couponInfo = "15% discount on all items " + productTypeValue + " is $ " + couponDiscount;
 
             let isCheaper = isBetterCoupon(couponDiscount);
             if (!isCheaper) {
